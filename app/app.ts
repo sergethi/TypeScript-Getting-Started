50302d3b12c01e79520a8e2fbe159a02b8a79ae3
sergethi <nsergearistide@gmail.com>
2022-03-04 17:38:15 +0000

    completed classes implementation

--- app/app.ts
+++ app/app.ts
@@ -1,54 +1,88 @@
-function startGame(){
-    //start a new game
+/// <reference path="player.ts" />
+/// <reference path="utility.ts" />
+/// <reference path="game.ts" />
 
-    let playerName: string | undefined = getInputValue('playername');
-    logPlayer(playerName)
+let newGame: Game;
 
-    postScore(90, playerName);
-    postScore(-9, playerName);
-    // var messagesElements = document.getElementById('messages');
-    // messagesElements!.innerText = 'Welcome to Multimath! Starting new game ...';
-}
+//add click handler to start game button
+document.getElementById('startGame')!.addEventListener('click', () => {
+    const player: Player = new Player();
+    player.name = Utility.getInputValue('playername');
 
-//assigning a default value to name makes it optional
-function logPlayer(name: string ='multiMath Player'): void {
-    console.log(`New game starting for player: ${name}`);
-}
+    const problemCount: number = Number(Utility.getInputValue('problemCount'));
+    const factor: number = Number(Utility.getInputValue('factor'));
 
-function getInputValue(elementID: string): string | undefined {
-    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    newGame = new Game(player, problemCount, factor);
+    newGame.displayGame();
+});
 
-    if(inputElement.value === ''){
-        return undefined;
-    }
-    else{
-        return inputElement.value;
-    }
-}
+//add click handler to calculate scrore button
+document.getElementById('calculate')!.addEventListener('click', () => {
+    newGame.calculateScore();
+});
 
-//adding '?' sign to playerName makes it optional
-//assigning a default value to PlayerName makes it optional
-function postScore(score: number, playerName: string = 'MultiMath Player'): void {
 
-    let logger: (value: string) => void;
+//*************************************************Not using Classes example*********************************************************** */
+// function startGame(){
+//     //start a new game
 
-    if(score < 0) {
-        logger = logError;
-    }
-    else{
-        logger = logMessage;
-    }
+//     let playerName: string | undefined = getInputValue('playername');
+//     logPlayer(playerName)
 
-    const scoreElement: HTMLElement | null =document.getElementById('postedScores');
-    scoreElement!.innerText = `${score} - ${playerName}`;
+//     postScore(90, playerName);
+//     postScore(-9, playerName);
+//     // var messagesElements = document.getElementById('messages');
+//     // messagesElements!.innerText = 'Welcome to Multimath! Starting new game ...';
+// }
 
-    logger(`Score: ${score}`);
-}
+// //assigning a default value to name makes it optional
+// function logPlayer(name: string ='multiMath Player'): void {
+//     console.log(`New game starting for player: ${name}`);
+// }
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
 
-const logMessage = (message: string) => console.log(message);
 
-function logError(err: string): void {
-    console.error(err);
-}
+// //adding '?' sign to playerName makes it optional
+// //assigning a default value to PlayerName makes it optional
+// function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+//     let logger: (value: string) => void;
+
+//     if(score < 0) {
+//         logger = logError;
+//     }
+//     else{
+//         logger = logMessage;
+//     }
+
+//     const scoreElement: HTMLElement | null =document.getElementById('postedScores');
+//     scoreElement!.innerText = `${score} - ${playerName}`;
+
+//     logger(`Score: ${score}`);
+// }
+
+// document.getElementById('startGame')!.addEventListener('click', startGame);
+
+// const logMessage = (message: string) => console.log(message);
+
+// function logError(err: string): void {
+//     console.error(err);
+// }
+
+// // Interfaces implementation using object literal
+// // let myResult: Result = {
+// //     playerName: "Papi",
+// //     score: 8,
+// //     problemCount: 6,
+// //     factor: 7
+// // };
+
+// // let player: Person = {
+// //     name: 'Daniella',
+// //     formatName: () => 'Dan'
+// // }
+
+//Player class instance
+// const firstPlayer: Player = new Player;
+// firstPlayer.name = 'Lanier';
+// console.log(firstPlayer.formatName());
---
+++ app/player.ts
@@ -0,0 +1,12 @@
+/// <reference path="person.ts" />
+
+class Player implements Person {
+    name!: string;
+    age!: number;
+    highScore!: number;
+
+    formatName(){
+        return this.name.toLocaleUpperCase();
+
+    }
+}
